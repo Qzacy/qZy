7236e06fdc4fb28407134aff80d22c960285c3e5
Qzacyer <47668628+Qzacy@users.noreply.github.com>
2019-12-23 06:17:05 -0500

    Update install.py

--- install.py
+++ install.py
@@ -33,5 +33,5 @@ if not to_install:
 else:
     print("Installing all requirements...")
     os.system("echo %s|sudo -S apt-get install %s" % (passwd, " ".join(to_install)))
-    os.system("pip install -r requirements.txt")
+    os.system("pip3 install -r requirements.txt")
     print("All requirements are installed.")
